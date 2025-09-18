2b1258aaab0baa9739b7aa9e8ba28f050e18f663
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2025-09-18 22:28:52 +0900

    initial setting

---
+++ src/main/java/com/knugpt/knugpt/KnugptApplication.java
@@ -0,0 +1,13 @@
+package com.knugpt.knugpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnugptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnugptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knugpt/knugpt/KnugptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knugpt.knugpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnugptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

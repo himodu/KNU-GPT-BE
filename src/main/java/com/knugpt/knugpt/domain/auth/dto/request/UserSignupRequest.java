7d7783eb5217ee26f41a9a2f2087bd90921651d0
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2025-11-13 23:28:49 +0900

    Update UserSignupRequest.java

--- src/main/java/com/knugpt/knugpt/domain/auth/dto/request/UserSignupRequest.java
+++ src/main/java/com/knugpt/knugpt/domain/auth/dto/request/UserSignupRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.knugpt.knugpt.domain.user.entity.UserStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
